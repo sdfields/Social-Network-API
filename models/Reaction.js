3457123f898238cf4d95599435a5a8ab431aaae0
sdfields <spencerdfields@gmail.com>
2023-04-03 00:18:42 -0500

    completed reactions model and passed array into thoughts

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,11 +1,36 @@
 // Imports / Requirements
 
+const moment = require('moment');
+const { Schema, Types } = require('mongoose');
+
 // Reaction
 
-// ID
+const reactionSchema = new Schema({
+    // ID
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    // Body
+    reactionBody: {
+        type: String,
+        required: true,
+        min_length: 1,
+        max_length: 280,
+    },
+    // Username
+    username: {
+        Type: String,
+        required: true,
+    },
+    // Created at
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: dateStamp => moment(dateStamp).format('MMM DD, YYYY [at] hh:mm a'),
+    },
+});
 
-// Body
+// Exports
 
-// Username
-
-// Created at
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,28 +1,48 @@
 // Imports / Requirements
 
 const { Schema, model } = require("mongoose");
-const moment = require('moment');
+const moment = require("moment");
+const reactionSchema = require("./Reaction");
 
 // Thought
 
-const thoughtSchema = new Schema({
-  // Text
-  thoughtText: {
-    type: String,
-    required: true,
-    min_length: 1,
-    max_length: 280,
+const thoughtSchema = new Schema(
+  {
+    // Text
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    // Created at
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (dateStamp) => moment(dateStamp).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    // Username
+    username: {
+      type: String,
+      required: true,
+    },
+    // Reactions
+    reactions: 
+    [reactionSchema],
   },
-  // Created at
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: dateStamp => moment(dateStamp).format('MM DD, YYYY [at] hh:mm a'),
-  },
-  // Username
-  username: {
-    type: String,
-    required: true,
-  },
-  // Reactions
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
 });
+
+// Exports
+
+const Thought = model('Thought', thoughtSchema);
+module.exports = Thought;
